fbf499a4d9355dd13291541ec69e1df629c670d9
Ильяс <ilas@MacBook-Air-Ilas.local>
2018-09-19 18:21:02 +0600

    Initial commit

---
+++ src/main/java/com/example/webproject/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.webproject.repos;
+
+import com.example.webproject.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+
+}
