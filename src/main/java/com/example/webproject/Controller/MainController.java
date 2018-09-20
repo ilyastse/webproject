1544e567645538d080396a4c6f3c768d7787d873
ilyastse <ilyas3194@mail.ru>
2018-09-20 21:59:43 +0600

    Add security settings

--- src/main/java/com/example/webproject/Controller/MainController.java
+++ src/main/java/com/example/webproject/Controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.webproject;
+package com.example.webproject.Controller;
 
 import com.example.webproject.domain.Message;
 import com.example.webproject.repos.MessageRepo;
@@ -13,17 +13,17 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
                            Map<String,Object> model) {
         model.put("name", name);
         return "greeting";
     }
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String,Object> model){
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -31,7 +31,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text,@RequestParam String tag,Map<String,Object> model){
         Message message = new Message(text, tag);
         messageRepo.save(message);
