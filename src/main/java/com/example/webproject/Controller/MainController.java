b60e4e347c5ab386ad15cba04f41da384d30d611
ilyastse <ilyas3194@mail.ru>
2018-09-21 20:49:31 +0600

    Add author

--- src/main/java/com/example/webproject/Controller/MainController.java
+++ src/main/java/com/example/webproject/Controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.webproject.Controller;
 
 import com.example.webproject.domain.Message;
+import com.example.webproject.domain.User;
 import com.example.webproject.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +34,12 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text,@RequestParam String tag,Map<String,Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String,Object> model){
+        Message message = new Message(text, tag,user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
 
