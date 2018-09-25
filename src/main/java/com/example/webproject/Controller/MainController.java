4e35dc17dbd86005477c25f176ce4b9a7b139761
ilyastse <ilyas3194@mail.ru>
2018-09-25 21:27:48 +0600

    Changed mustache on FreeMarker Template

--- src/main/java/com/example/webproject/Controller/MainController.java
+++ src/main/java/com/example/webproject/Controller/MainController.java
@@ -26,10 +26,16 @@ public class MainController {
         return "greeting";
     }
     @GetMapping("/main")
-    public String main(Map<String,Object> model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -45,20 +51,6 @@ public class MainController {
 
         model.put("messages", messages);
 
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter , Map<String,Object> model){
-        Iterable<Message> messages;
-
-        if(filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        }else{
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
         return "main";
     }
 
